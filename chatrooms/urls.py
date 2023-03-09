dab85cf20d72b775c448d333647162df074b5b43
EstivenValencia <estivenvalenciacastrillon@gmail.com>
2023-03-09 16:23:49 -0500

    4

--- chatrooms/urls.py
+++ chatrooms/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
 	path('', Index.as_view(), name='index'),
 	path('rooms/', RetrieveRoom.as_view()),
-	path('<str:room_name>/', Room.as_view(), name='room'),
+	path('<user_id_from>/<user_id_to>', Room.as_view(), name='room'),
 ]
